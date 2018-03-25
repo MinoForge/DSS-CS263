99106c34aad8eafb31020bc5a32051baff0c4d17
Brandon T <bc_townsend@outlook.com>
2018-03-25 16:34:20 -0400

    RefArg is almost done. Need to complete Argument first to figure out the rating system.

--- src/refmeister/entity/RefArg.java
+++ src/refmeister/entity/RefArg.java
@@ -1,26 +1,100 @@
 package refmeister.entity;
 
+/**
+ * This is an intermediate class modeling the links associating a Reference and an Argument.
+ * @author Red Team/DevSquad Supreme
+ * @version 25, 3, 2018
+ */
 class RefArg {
 
+	/** The Reference to which this object is associating an Argument to. */
+	private Reference reference;
+	/** The Argument which this object is associating a Reference to. */
+	private Argument argument;
+	/** The rating for a given argument. */
 	private float rating;
 
-	public void getRating() {
-		// TODO - implement RefArg.getRating
-		throw new UnsupportedOperationException();
+	/**
+	 * Constructor for a RefArg object. Links a specified Reference to a specified Argument and
+	 * sets a rating for link.
+	 * @param ref The specified Reference to be linked to an Argument.
+	 * @param argument The specified Argument to be linked to a Reference.
+	 * @param rating The rating for this specified argument link.
+	 */
+	public RefArg(Reference ref, Argument argument, float rating) {
+		this.reference = ref;
+		this.argument = argument;
+		this.rating = rating;
 	}
 
 	/**
-	 * 
-	 * @param rating
+	 * Retrieves the Reference that is associated with this RefArg's argument
+	 * @return The Reference that is associated with this RefArg's argument.
 	 */
-	public void setRating(int rating) {
-		// TODO - implement RefArg.setRating
-		throw new UnsupportedOperationException();
+	public Reference getReference() {
+		return this.reference;
 	}
 
+	/**
+	 * Associates this RefArg's argument with a new specified reference.
+	 * @param reference The new specified reference.
+	 */
+	public void setReference(Reference reference) {
+		this.reference = reference;
+	}
+
+	/**
+	 * Retrieves the Argument that is associated with this RefArg's reference.
+	 * @return The Argument that is associated with this RefArg's reference.
+	 */
+	public Argument getArgument() {
+		return this.argument;
+	}
+
+	/**
+	 * Associates this RefArg's reference with a new specified argument.
+	 * @param argument The new specified argument.
+	 */
+	public void setArgument(Argument argument) {
+		this.argument = argument;
+	}
+
+	/**
+	 * Retrieves the rating of this single RefArg.
+	 * @return The rating of this single RefArg.
+	 */
+	public float getRating() {
+		return this.rating;
+	}
+
+	/**
+	 * Sets the rating of this RefArg to a new specified rating.
+	 * @param rating The new specified rating.
+	 */
+	public void setRating(float rating) {
+		this.rating = rating;
+	}
+
+	public float getRatingSum() {
+		//TODO have to get value of each RefArg's rating.
+		for(RefArg ra : this.argument.)
+	}
+
+	/**
+	 * Looks in both the Reference and Argument that are linked to this RefArg. When it finds the
+	 * RefArg that links them, it will remove the RefArg from that ArrayList.
+	 */
 	public void destroy() {
-		// TODO - implement RefArg.destroy
-		throw new UnsupportedOperationException();
+		for(RefArg ra : reference.getArguments()) {
+			if(this == ra) {
+				reference.getArguments().remove(this);
+			}
+		}
+		for(RefArg ra : argument.getRefs()) {
+			if(this == ra) {
+				argument.getRefs().remove(this);
+			}
+		}
 	}
 
 }

e7684a942aeda79ed1e50b51b041fd52467e60cc
Brandon T <bc_townsend@outlook.com>
2018-03-26 03:20:25 -0400

    javadoc for Editable.

--- src/refmeister/entity/Editable.java
+++ src/refmeister/entity/Editable.java
@@ -7,22 +7,26 @@ import java.security.InvalidParameterException;
 import java.util.List;
 
 /**
- * @author
- * @version
+ * Editable is an abstract class which our other classes will implement. If the child class has a
+ * title, description, etc. it will extend Editable.
+ * @author Peter Gardner
+ * @version 26, 3, 2018
  */
 public abstract class Editable implements Displayable, Saveable {
 
-    /**  */
+    /** The title for any Editable objects. */
 	private String title;
-	/**  */
+	/** The description for any Editable objects. */
 	private String description;
-	/**  */
+	/** The parent of an Editable object if it is specified to have a parent. */
 	private Editable parent;
 
     /**
-     *
-     * @param attribute
-     * @return
+     * Retrieves either the title, description, or null depending on the String passed to the
+     * method.
+     * @param attribute The String that determines what attribute to choose.
+     * @return this.getTitle() if attribute is "title", this.getDescription() if attribute is
+     * "description", or null otherwise.
      */
     public String getAttribute(String attribute){
         if(attribute.equals("title")){
@@ -36,9 +40,10 @@ public abstract class Editable implements Displayable, Saveable {
     }
 
     /**
-     *
-     * @param attribute
-     * @param contents
+     * Sets contents to a specified attribute, if that attribute is "title" or "description". If
+     * it is not either, it will throw an InvalidParameterException.
+     * @param attribute The String that specifies what should be set to the value of contents.
+     * @param contents The value to be set to a specified attribute.
      */
     public void setAttribute(String attribute, String contents){
         if(attribute.equals("title")){
@@ -52,48 +57,48 @@ public abstract class Editable implements Displayable, Saveable {
     }
 
     /**
-     *
-     * @return
+     * Retrieves this Editable's parent.
+     * @return This Editable's parent.
      */
     public Editable getParent() {
         return parent;
     }
 
     /**
-     *
-     * @param parent
+     * Sets this Editable's parent.
+     * @param parent The new Editable to be set to this.parent.
      */
     public void setParent(Editable parent) {
         this.parent = parent;
     }
 
     /**
-     *
-     * @return
+     * Retrieves this Editable's title.
+     * @return This Editable's title.
      */
     public String getTitle() {
         return title;
     }
 
     /**
-     *
-     * @param title
+     * Sets this Editable's title.
+     * @param title The new String to be set to this.title.
      */
     public void setTitle(String title) {
         this.title = title;
     }
 
     /**
-     *
-     * @return
+     * Retrieves this Editable's description.
+     * @return This Editable's description.
      */
     public String getDescription() {
         return description;
     }
 
     /**
-     *
-     * @param description
+     * Sets this Editable's description.
+     * @param description The new String to be set to this.description.
      */
     public void setDescription(String description) {
         this.description = description;
@@ -102,7 +107,7 @@ public abstract class Editable implements Displayable, Saveable {
     /**
      * Creates an XML tag for an editable object, given it's tag name.
      * @param tagName   The tag name of this tag.
-     * @param manager
+     * @param manager   The XMLManager that this traversal is being used with.
      * @return          A formatted XML String.
      */
     protected String getSaveString(String tagName, XMLManager manager){
@@ -124,8 +129,9 @@ public abstract class Editable implements Displayable, Saveable {
     }
 
     /**
-     *
-     * @param edits
+     * Sets title and description to the indices in edits.
+     * @param edits An array of Strings which contains the new title and description for this
+     *              Editable.
      */
     public void edit(String[] edits) {
         setTitle(edits[0]);
@@ -135,7 +141,7 @@ public abstract class Editable implements Displayable, Saveable {
     /**
      * Gets an array of elements that this object has, with title being at
      * index 0, description at index 1, and each child as a sub index.
-     * @return
+     * @return An array of Strings to be displayed by the Controller.
      */
     public String[] display() {
         String[] display = new String[7 + getChildren().size()];
@@ -154,16 +160,16 @@ public abstract class Editable implements Displayable, Saveable {
     }
 
     /**
-     *
-     * @param title
-     * @param description
-     * @return
+     * Creates a child for this Editable.
+     * @param title The title for the child.
+     * @param description The description for the child.
+     * @return true if the child was able to be created, false otherwise.
      */
     public abstract boolean createChild(String title, String description);
 
     /**
-     *
-     * @return
+     * Retrieves the list of this Editable's children.
+     * @return The list of this Editable's children.
      */
     public abstract List<Editable> getChildren();
 }

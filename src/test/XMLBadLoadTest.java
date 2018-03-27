c92382bc0497063c92246e426951215d014ac510
Wesley Rogers <wsrogers3@catamount.wcu.edu>
2018-03-26 23:40:42 -0400

    Added some more xml stuff

---
+++ src/test/XMLBadLoadTest.java
@@ -0,0 +1,24 @@
+package test;
+
+import refmeister.XML.XMLParser;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ * Tests a bad XML file
+ */
+public class XMLBadLoadTest {
+    public static void main(String[] args){
+        String badXML = "";
+        try (Scanner scanner = new Scanner(new File("bad_xml/test1.rl"))) {
+            scanner.useDelimiter("\\Z");
+            badXML = scanner.next();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        XMLParser.loadLibrary(badXML);
+    }
+}

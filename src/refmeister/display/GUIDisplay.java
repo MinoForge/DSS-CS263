31e4a66eb2c590240551dfe14b971642f8b91666
Caleb <cwdinehart1@catamount.wcu.edu>
2018-04-28 21:14:44 -0400

    TitleDescPane

--- src/refmeister/display/GUIDisplay.java
+++ src/refmeister/display/GUIDisplay.java
@@ -11,6 +11,7 @@ import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.util.Pair;
 import refmeister.XML.FileManager;
 import refmeister.controllers.Controller;
 import refmeister.controllers.SingleLibraryController;
@@ -28,6 +29,8 @@ public class GUIDisplay extends Application implements Displayer{
 
     private Controller control;
     private Stage theStage;
+    private TitleDescriptionPane titleDescPane;
+
 
     public GUIDisplay(){
         this.control = null;
@@ -84,7 +87,47 @@ public class GUIDisplay extends Application implements Displayer{
         });
 
         newButton.setOnAction((ev) -> {
-            control.createLibrary();
+            Dialog<Pair<String, String>> dialog = new Dialog();
+            dialog.setHeaderText("Enter Information");
+            ButtonType done = new ButtonType("Done", ButtonBar.ButtonData.OK_DONE);
+            dialog.getDialogPane().getButtonTypes().addAll(done, ButtonType.CANCEL);
+
+            GridPane grid = new GridPane();
+            grid.setHgap(2);
+            grid.setVgap(1);
+           // grid.setPadding(new Insets(20, 150, 10,10));
+
+            TextField title1 = new TextField();
+            title1.setPromptText("Title");
+            TextField description = new TextField();
+            description.setPromptText("Description");
+            description.setMinSize(250, 100);
+
+            grid.add(new Text("Title:"), 0, 0,1 ,1);
+            grid.add(title1, 1,0, 1, 1);
+            grid.add(new Text("Description:"), 0, 1, 1, 1);
+            grid.add(description, 1, 1, 1, 1);
+
+            RowConstraints row1 = new RowConstraints();
+            row1.setPercentHeight(50);
+            RowConstraints row2 = new RowConstraints();
+            row2.setPercentHeight(50);
+
+            ColumnConstraints col1 = new ColumnConstraints();
+            col1.setPercentWidth(25);
+            ColumnConstraints col2 = new ColumnConstraints();
+            col2.setPercentWidth(75);
+
+            grid.getColumnConstraints().addAll(col1, col2);
+            grid.getRowConstraints().addAll(row1, row2);
+
+            dialog.getDialogPane().setContent(grid);
+
+            dialog.showAndWait();
+
+
+            control.createLibrary(title1.getText(), description.getText());
+            //this.update();
             openApp();
         });
 
@@ -113,7 +156,12 @@ public class GUIDisplay extends Application implements Displayer{
         multiList.createTabs("White", "Brown", "Black");
         multiList.prefWidthProperty().bind(root.widthProperty());
         multiList.prefHeightProperty().bind(root.heightProperty());
-        root.setCenter(multiList);
+
+        VBox centerBox = new VBox();
+
+        centerBox.getChildren().addAll( multiList);
+
+        root.setCenter(centerBox);
 
         root.setTop(getMenuBar());
 
@@ -194,4 +242,8 @@ public class GUIDisplay extends Application implements Displayer{
         out.getMenus().addAll(file, edit, help);
         return out;
     }
+//    @Override
+//    public void update() {
+//
+//    }
 }
--- src/refmeister/display/elements/TitleDescriptionPane.java
+++ src/refmeister/display/elements/TitleDescriptionPane.java
@@ -1,7 +1,87 @@
 package refmeister.display.elements;
 
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
-import javafx.scene.layout.TilePane;
+public class TitleDescriptionPane extends VBox {
 
-public class TitleDescriptionPane extends TilePane {
+    private Label title;
+
+    private Label desc;
+
+    private Label rating;
+
+    private OptionsPane option;
+
+    private static TitleDescriptionPane obj;
+
+    /**
+     * Default constructor for a TitleDescriptionPane object.
+     */
+    private TitleDescriptionPane(){
+        this(null, "TITLE", "DESCRIPTION");
+    }
+
+    private TitleDescriptionPane(OptionsPane option){
+        this(option, "TITLE","DESCRIPTION");
+    }
+
+    private TitleDescriptionPane(OptionsPane option ,String... attributes) {
+        AnchorPane anchor = new AnchorPane();
+        this.option = option;
+
+        if(attributes.length < 3){
+            this.title = new Label(attributes[0]);
+            this.desc = new Label(attributes[1]);
+            AnchorPane.setLeftAnchor(this.title, 10.0);
+            AnchorPane.setRightAnchor(this.option, 10.0);
+            anchor.getChildren().addAll(this.title, this.option);
+            this.getChildren().addAll(anchor, this.desc);
+        }
+        else {
+            this.title = new Label(attributes[0]);
+            this.desc = new Label(attributes[1]);
+            this.rating = new Label(attributes[2]);
+            AnchorPane.setLeftAnchor(this.title, 10.0);
+            AnchorPane.setRightAnchor(this.option, 10.0);
+            anchor.getChildren().addAll(this.title, this.option);
+            this.getChildren().addAll(anchor, this.desc, this.rating);
+        }
+        obj = this;
+
+    }
+
+
+    /**
+     * Returns the current instance of TitleDescriptionPane.
+     * @return the current instance of TitleDescriptionPane.
+     */
+    public static TitleDescriptionPane getInstance() {
+        if(obj == null) {
+            synchronized (Pane.class) {
+                if(obj == null) {
+                    obj = new TitleDescriptionPane();
+                }
+            }
+        }
+        return obj;
+    }
+
+    public void setTitleDescPane(String[] attributes, OptionsPane options) {
+        for(int i = 0; i < attributes.length; i++){
+            if(i == 0)
+                this.title = new Label(attributes[i]);
+            if(i == 1)
+                this.desc = new Label(attributes[i]);
+            if(i == 2)
+                this.rating = new Label(attributes[i]);
+        }
+
+        this.option = options;
+    }
+
 }
